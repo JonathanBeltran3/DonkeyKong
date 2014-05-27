470e069562413ba0edb533799dce356fa6017756
AngelZatch <pinbouen.andreas@gmail.com>
2014-05-27 20:14:07 +0200

    Mario.h

--- Windows/includes/mario.h
+++ Windows/includes/mario.h
@@ -2,8 +2,16 @@
 #define MARIO_H_INCLUDED
 
 typedef struct Mario{
-
-
+    float x;
+    float y;
+    int health;
+    float speedX;
+    float speedY;
+    float acceleration;
+    float width;
+    float height;
 } Mario;
+
+Mario createMario(float x, float y, int health, float speedX, float speedY, float acceleration, float width, float height);
 
 #endif // MARIO_H_INCLUDED
