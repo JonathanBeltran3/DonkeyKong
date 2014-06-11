943a96ce7b5f9b04d3e9f0073445ffef50c74200
JonathanBeltran3 <you@example.com>
2014-06-11 15:11:01 +0200

    test1
    test1

---
+++ MacDonkeyV2/MacDonkeyV2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MacDonkeyV2
+//
+//  Created by Jo' on 11/06/2014.
+//  Copyright (c) 2014 azer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
